f9177f73c53250eb4bb54c06197bb5c4f08aee68
Qihong Lu <lvqihong1992@gmail.com>
2016-05-18 14:21:51 -0500

    some comments

--- src/myStepFunction.m
+++ src/myStepFunction.m
@@ -1,6 +1,8 @@
 function [ output ] = myStepFunction( input )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+% step function: R^n -> R^n 
+% f(x_i) =  1, if x_i >= 0 
+%           0, if x_i < 0 
+% for all i 
 
 output = nan(length(input),1);
 output(input >= 0) = 1;  % the point zero has measure zero anyway
