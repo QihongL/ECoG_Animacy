47211a124e434864dd2747b29cce1a6dbb3592dd
Qihong Lu <lvqihong1992@gmail.com>
2016-08-19 12:45:50 -0700

    plot y coords for all subjs

--- src/iterLasso_temporal_plot.m
+++ src/iterLasso_temporal_plot.m
@@ -142,9 +142,8 @@ yCoords.raw.median = median(yCoords.raw.all);
 
 %% gather data , loop over subjects 1st 
 legend_subjIDs.ref = cell(nSubjs.ref,1);
-weightedCoord.ref = cell(numCVB,1);
 legend_subjIDs.raw = cell(nSubjs.raw,1);
-weightedCoord.raw = cell(numCVB,1);
+weightedCoord = cell(numCVB,1);
 for s = 1 : nSubjs.ref
     curSubjID = allSubjIDs.ref(s);
     % get the legend for subject id
@@ -155,11 +154,10 @@ for s = 1 : nSubjs.ref
         s, numCVB, yCoords.ref, featureSelected.ref);
     % unpack the data
     for c = 1 : numCVB
-        weightedCoord.ref{c}(:,s) = weightedCoord_singleSub{c};
+        weightedCoord{c}.ref(:,s) = weightedCoord_singleSub{c};
     end
     
 end
-
 for s = 1 : nSubjs.raw
     curSubjID = allSubjIDs.raw(s);
     % get the legend for subject id
@@ -170,7 +168,7 @@ for s = 1 : nSubjs.raw
         s, numCVB, yCoords.raw, featureSelected.raw);
     % unpack the data
     for c = 1 : numCVB
-        weightedCoord.raw{c}(:,s) = weightedCoord_singleSub{c};
+        weightedCoord{c}.raw(:,s) = weightedCoord_singleSub{c};
     end
     
 end
@@ -186,11 +184,10 @@ p.FS = 14;
 p.LW = 2;
 
 % set the legend for all participants 
-
 for s = 1 : nSubjs.all
     legend_subjIDs.all{s} = num2str(allSubjIDs.all(s));
 end
-
+%% combine ref and raw data, prefer ref
 missingSubjIdx = 6;
 propFeatureSelected.all = nan(length(allTps), nSubjs.all);
 propFeatureSelected.all = collapse_ref_raw(propFeatureSelected, allSubjIDs, subjIdx, missingSubjIdx);
@@ -204,6 +201,10 @@ wtsVal_all.all = collapse_ref_raw(wtsVal_all, allSubjIDs, subjIdx, missingSubjId
 wtsVal_nz.all = nan(length(allTps), nSubjs.all);
 wtsVal_nz.all = collapse_ref_raw(wtsVal_nz, allSubjIDs, subjIdx, missingSubjIdx);
 
+for c = 1 : numCVB
+    weightedCoord{c}.all = nan(length(allTps), nSubjs.all);
+    weightedCoord{c}.all = collapse_ref_raw(weightedCoord{c}, allSubjIDs, subjIdx, missingSubjIdx);
+end
 
 %% proportion of features selected over time
 figure(1)
@@ -239,38 +240,38 @@ ylabel('% features selected', 'fontsize', p.FS)
 xlabel('Time (Unit of 10ms)', 'fontsize', p.FS)
 
 
-% %% number of lasso iterations over time (indicate multi-collinearity)
-% figure(2)
-% 
-% subplot(3,1,1)
-% plot(allTps, numIterations.all, 'linewidth', p.LW)
-% xlim([0 allTps(end)])
-% legend(legend_subjIDs.all, 'fontsize', p.FS, 'location', 'NW')
-% title_text = sprintf('Feature multicollinearity over time');
-% title(title_text, 'fontsize', p.FS);
-% ylabel('Number of lasso iterations', 'fontsize', p.FS)
-% 
-% 
-% subplot(3,1,2)
-% plot(allTps, movingmean(numIterations.all, 3), 'linewidth', p.LW)
-% 
-% legend(legend_subjIDs.all, 'fontsize', p.FS, 'location', 'NW')
-% xlim([0 allTps(end)])
-% title_text = sprintf('Moving average window size = 3');
-% title(title_text, 'fontsize', p.FS);
-% ylabel('Number of lasso iterations', 'fontsize', p.FS)
-% 
-% subplot(3,1,3)
-% se = tinv(.975, nSubjs.all-1) * std(numIterations.all, 0, 2) / sqrt(nSubjs.all);
-% hold on
-% errorbar(allTps, mean(numIterations.all,2), se, 'linewidth', p.LW)
-% plot([0 allTps(end)],[0 0], '--k')
-% hold off
-% xlim([0 allTps(end)])
-% title('Averaged across subjects', 'fontsize', p.FS);
-% ylabel('Number of lasso iterations', 'fontsize', p.FS)
-% xlabel('Time (Unit of 10ms)', 'fontsize', p.FS)
-% 
+%% number of lasso iterations over time (indicate multi-collinearity)
+figure(2)
+
+subplot(3,1,1)
+plot(allTps, numIterations.all, 'linewidth', p.LW)
+xlim([0 allTps(end)])
+legend(legend_subjIDs.all, 'fontsize', p.FS, 'location', 'NW')
+title_text = sprintf('Feature multicollinearity over time');
+title(title_text, 'fontsize', p.FS);
+ylabel('Number of lasso iterations', 'fontsize', p.FS)
+
+
+subplot(3,1,2)
+plot(allTps, movingmean(numIterations.all, 3), 'linewidth', p.LW)
+
+legend(legend_subjIDs.all, 'fontsize', p.FS, 'location', 'NW')
+xlim([0 allTps(end)])
+title_text = sprintf('Moving average window size = 3');
+title(title_text, 'fontsize', p.FS);
+ylabel('Number of lasso iterations', 'fontsize', p.FS)
+
+subplot(3,1,3)
+se = tinv(.975, nSubjs.all-1) * std(numIterations.all, 0, 2) / sqrt(nSubjs.all);
+hold on
+errorbar(allTps, mean(numIterations.all,2), se, 'linewidth', p.LW)
+plot([0 allTps(end)],[0 0], '--k')
+hold off
+xlim([0 allTps(end)])
+title('Averaged across subjects', 'fontsize', p.FS);
+ylabel('Number of lasso iterations', 'fontsize', p.FS)
+xlabel('Time (Unit of 10ms)', 'fontsize', p.FS)
+
 
 
 %% average weight magnitude
@@ -342,28 +343,34 @@ xlabel('Time (Unit of 10ms)', 'fontsize', p.FS)
 % ylabel('Number of electrodes', 'fontsize', 14)
 % title('Distribution of electrodes across all subjects', 'fontsize', 14)
 
+% plot individual subject 
 tempSubPlotIdx = 1;
 figure(4)
 increment = 3;
 for idx = 2 : 2: numCVB
     subplot(5,increment,tempSubPlotIdx)
     
-    plot(allTps, weightedCoord.ref{idx}, 'linewidth', p.LW)
+    plot(allTps, weightedCoord{idx}.all, 'linewidth', p.LW)
     title_text = sprintf('CVB threshold = %d', idx);
     title(title_text, 'fontsize', p.FS);
     ylabel('Pos <---> Ant', 'fontsize', p.FS)
     
     xlim([0 allTps(end)])
+    ylim([-20 20])
     tempSubPlotIdx = tempSubPlotIdx + increment;
+    if idx == 2 
+        legend(legend_subjIDs.all, 'fontsize', p.FS, 'location', 'NW')
+    end
 end
 xlabel('Time (unit of 10 ms)', 'fontsize', p.FS)
 
+% plot individual subject WITH MOVING WINDOW AVERAGING
 movingWindowSize = 5;
 tempSubPlotIdx = 2;
 for idx = 2 : 2: numCVB
     subplot(5,increment,tempSubPlotIdx)
     
-    plot(allTps, movingmean(weightedCoord.ref{idx}, 5), 'linewidth', p.LW)
+    plot(allTps, movingmean(weightedCoord{idx}.all, 5), 'linewidth', p.LW)
     title_text = sprintf('Moving window size = %d', movingWindowSize);
     if idx == 2
         title(title_text, 'fontsize', p.FS);
@@ -371,17 +378,20 @@ for idx = 2 : 2: numCVB
     %     ylabel('Pos <---> Ant', 'fontsize', p.FS)
     
     xlim([0 allTps(end)])
+    ylim([-20 20])
     tempSubPlotIdx = tempSubPlotIdx + increment;
 end
 xlabel('Time (unit of 10 ms)', 'fontsize', p.FS)
 
+
+% plot mean curve 
 tempSubPlotIdx = 3;
 for idx = 2 : 2: numCVB
     subplot(5,increment,tempSubPlotIdx)
     
-    se = std(weightedCoord.ref{idx},0,2) * tinv(.975, nSubjs.ref - 1);
-    plot(allTps, mean(weightedCoord.ref{idx},2), 'linewidth', p.LW)
-    %     errorbar(allTps, mean(weightedCoord.ref{i},2), se, 'linewidth', p.LW)
+    se = std(weightedCoord{idx}.all,0,2) * tinv(.975, nSubjs.ref - 1);
+%     plot(allTps, mean(weightedCoord{idx}.all,2), 'linewidth', p.LW)
+    errorbar(allTps, mean(weightedCoord{idx}.all,2), se, 'linewidth', p.LW)
     title_text = sprintf('Average');
     if idx == 2
         title(title_text, 'fontsize', p.FS);
@@ -389,6 +399,7 @@ for idx = 2 : 2: numCVB
     %     ylabel('Pos <---> Ant', 'fontsize', p.FS)
     
     xlim([0 allTps(end)])
+    ylim([-20 20])
     tempSubPlotIdx = tempSubPlotIdx + increment;
 end
 xlabel('Time (unit of 10 ms)', 'fontsize', p.FS)
